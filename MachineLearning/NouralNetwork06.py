ab0ab0e8dbb1d502f52807ca1aa1df07475cc478
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-09-24 00:45:11 +0530

    Exception Handling

--- MachineLearning/NouralNetwork06.py
+++ MachineLearning/NouralNetwork06.py
@@ -6,8 +6,19 @@ X = [[1, 2, 3, 4],
 
 
 class Layer_Dense:
-    def __init__(self):
-        pass
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-    def forward(self):
-        pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 4)
+
+layer1.forward(X)
+
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
