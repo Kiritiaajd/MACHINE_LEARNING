4475ad66a0e789c0ccf76f1c3989184aa30b13e5
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-09-23 14:08:06 +0530

    Exception Handling

--- MachineLearning/NeuralNetwork03.py
+++ MachineLearning/NeuralNetwork03.py
@@ -1,10 +1,15 @@
 import numpy as np
 
+# Correcting the number of biases
 inputs = [1, 2, 3, 4, 5, 6]
 weights = [[0.1, 2.1, -3.9, 4.8, -9.0, 0.6],
            [0.1, 1.1, -3.9, 3.8, -3.0, 7.6],
            [0.1, 9.1, -3.9, 6.8, 4.5, 3.6]]
 
-biases = [2.8, 3.1, 4.9, 2.4]
+# Adjust biases to match the number of output neurons
+biases = [2.8, 3.1, 4.9]  # Should match the number of weight rows
 
-output = np.dot(inputs, weights) + biases
+# Perform the dot product and add biases
+output = np.dot(weights, inputs) + biases
+
+print(output)
