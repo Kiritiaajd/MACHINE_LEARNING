826672bbee2924476901c5c90054a1b0d9e260ab
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-09-23 11:36:53 +0530

    NeuralNetwork01

---
+++ MachineLearning/NeuralNetwork01.py
@@ -0,0 +1,12 @@
+inputs  = [1 ,2,3 ,4 ,5 ,6]
+weights = [0.1 , 2.1 , -3.9 , 4.8 , -9.0, 7.6]
+bias = 1
+output = 0
+for  i in range(len(inputs)):
+    output = inputs[i] * weights[i] + bias
+
+
+print(output)
+actual_output = [2 , 3, 4,6,1,7]
+
+
