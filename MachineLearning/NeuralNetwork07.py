4dc1c0fcdcfa1fc9a38dc5fc0de252ea50e03bd0
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-09-24 11:11:33 +0530

    Exception Handling

---
+++ MachineLearning/NeuralNetwork07.py
@@ -0,0 +1,30 @@
+import numpy as np
+from nnfs.console import nnfs
+
+np.random.seed(0)
+
+X = [[1, 2, 3, 4],
+     [0.2, 0.3, 0.4, 0.5],
+     [-0.26, 0.34, -2.1, -0.9]]
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
